af2cf9b37534b201ef26cbd08eeb88cfd8f8067a
haukeeckel <Haukeeckel@posteo.de>
2021-11-12 18:21:51 +0100

    Lab solved

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,23 @@
 // Iteration #1
+const { Schema, model } = require('mongoose');
+
+const DroneSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  propellers: {
+    type: Number,
+    require: true,
+    min: 1,
+  },
+  maxSpeed: {
+    type: Number,
+    require: true,
+    min: 1,
+  },
+});
+
+const DroneModel = model('Drone', DroneSchema);
+
+module.exports = DroneModel;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
 // Iteration #1
+require('../db');
+
+let DroneModel = require('../models/Drone.model');
+const mongoose = require('mongoose');
+
+const drones = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+];
+
+DroneModel.insertMany(drones)
+  .then(() => {
+    console.log(`${drones.length} Drones added`);
+    mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.log('Error', err);
+    mongoose.connection.close();
+  });
